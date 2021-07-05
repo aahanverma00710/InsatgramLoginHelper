5e7a9c86d2a79a6fd287f2adbdc2a913fd64859d
aahan verma <aahan.verma@netsetsoftware.com>
2021-07-05 10:01:23 +0530

    Bug Fixed on login error

--- instagramlogin/src/main/java/com/login/instagramlogin/helper/InstagramLoginHelper.kt
+++ instagramlogin/src/main/java/com/login/instagramlogin/helper/InstagramLoginHelper.kt
@@ -13,6 +13,7 @@ import android.webkit.WebViewClient
 import androidx.annotation.RequiresApi
 import com.login.instagramlogin.R
 import com.login.instagramlogin.appinterface.OnInstLoginListener
+import com.login.instagramlogin.async.LoadRedirectUrlForInsta
 import com.login.instagramlogin.utils.LoginOptions
 
 object InstagramLoginHelper {
@@ -29,16 +30,19 @@ object InstagramLoginHelper {
         dialogWeb.setCanceledOnTouchOutside(true)
         dialogWeb.setContentView(R.layout.dialog_web_view)
         dialogWeb.window!!.setLayout(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
         )
         val webView = dialogWeb.findViewById(R.id.webView) as WebView
         webView.isVerticalScrollBarEnabled = false
         webView.isHorizontalScrollBarEnabled = false
         webView.settings.layoutAlgorithm = WebSettings.LayoutAlgorithm.NORMAL;
 
-        dialogWeb.window!!.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
-        webView.webViewClient = MyLoginListenerWebViewClient(context,dialogWeb,loginOptions)
+        dialogWeb.window!!.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        webView.webViewClient = MyLoginListenerWebViewClient(context, dialogWeb, loginOptions)
         webView.settings.javaScriptEnabled = true
         webView.clearCache(true)
         webView.clearFormData()
@@ -46,8 +50,13 @@ object InstagramLoginHelper {
         webView.loadUrl(loginOptions.getInstaAuthUrl())
         dialogWeb.show()
     }
-    class MyLoginListenerWebViewClient(val activity: Context, val dialogWeb: Dialog, val options: LoginOptions) :
-            WebViewClient() {
+
+    class MyLoginListenerWebViewClient(
+        val activity: Context,
+        val dialogWeb: Dialog,
+        val options: LoginOptions
+    ) :
+        WebViewClient() {
 
 
         override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
@@ -58,7 +67,7 @@ object InstagramLoginHelper {
         @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
         override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
             if (request.url.toString().startsWith(options.getRedirectionUrl())) {
-                startAsyncTaskForLogin(request.url.toString(), activity, dialogWeb,options)
+                startAsyncTaskForLogin(request.url.toString(), activity, dialogWeb, options)
                 return true
             }
             return false
@@ -66,7 +75,7 @@ object InstagramLoginHelper {
 
         override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
             if (url.startsWith("https")) {
-                startAsyncTaskForLogin(url, activity, dialogWeb,options)
+                startAsyncTaskForLogin(url, activity, dialogWeb, options)
                 return true
             }
             return false
@@ -77,14 +86,20 @@ object InstagramLoginHelper {
             super.onPageFinished(view, url)
 
 
-
         }
 
     }
 
-    fun startAsyncTaskForLogin(url: String, activity: Context, dialogWeb: Dialog,options: LoginOptions) {
+    fun startAsyncTaskForLogin(
+        url: String,
+        activity: Context,
+        dialogWeb: Dialog,
+        options: LoginOptions
+    ) {
         if (url.contains("code")) {
-          //  MyAsyncTask(code, activity, dialogWeb,listener).execute()
+            val temp = url.split("=").toTypedArray()
+            val code = temp[1]
+            LoadRedirectUrlForInsta(code, activity, dialogWeb, options).execute()
         } else if (url.contains("error")) {
             val temp = url.split("=").toTypedArray()
             options.getLoginListener().onFailure("Something Went Wrong Please try again")
